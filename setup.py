9a776b5e244c8903b9cd2b2fe8e1915abb2fb8c9
Aitirga <aitirga@gmail.com>
2019-10-09 22:36:13 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='ANN-solver',
+    version='0.9',
+    packages=['tests', 'core_files'],
+    url='https://github.com/aitirga/ANN_solver',
+    license='',
+    author='Aitirga',
+    author_email='aitirga@gmail.com',
+    description='Artificial Neural Network (ANN) solver'
+)
